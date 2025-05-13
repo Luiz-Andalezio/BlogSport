badfeada0a311ced22e9b6178c841529558e7042
Luiz-Andalezio <luizandalecio19@gmail.com>
2025-05-13 18:17:22 -0300

    feat(form): Create the form to generate new posts

--- src/main/java/com/spring/blogsport/controller/BlogsportController.java
+++ src/main/java/com/spring/blogsport/controller/BlogsportController.java
@@ -1,17 +1,22 @@
 package com.spring.blogsport.controller;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.spring.blogsport.model.Post;
 import com.spring.blogsport.service.BlogsportService;
 
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import jakarta.validation.Valid;
+
 
 @Controller
 public class BlogsportController {
@@ -34,4 +39,21 @@ public class BlogsportController {
         mv.addObject("post", post);
         return mv;
     }
+
+    @RequestMapping(value = "/newpost", method=RequestMethod.GET)
+    public String getPostForm() {
+        return "postForm";
+    }
+
+    @RequestMapping(value = "/newpost", method=RequestMethod.POST)
+    public String savePost(@Valid Post post, BindingResult result, RedirectAttributes attributes) {
+        if (result.hasErrors()) {
+            attributes.addFlashAttribute("error", "Please fill in all fields.");
+            return "redirect:/newpost";
+        }
+        post.setDate(LocalDate.now());
+        blogsportService.save(post);
+        attributes.addFlashAttribute("success", "Post created successfully!");
+        return "redirect:/posts";
+    }    
 }
