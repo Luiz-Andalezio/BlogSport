e4d5f27736de18c436f694bc8adf2d07576842fb
Luiz-Andalezio <luizandalecio19@gmail.com>
2025-05-17 13:03:03 -0300

    fix(posts): Fixes display of posts stored in the database

--- src/main/java/com/spring/blogsport/controller/HomeController.java
+++ src/main/java/com/spring/blogsport/controller/HomeController.java
@@ -1,12 +1,24 @@
 package com.spring.blogsport.controller;
 
+import com.spring.blogsport.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+
+    private final PostService postService;
+
+    @Autowired
+    public HomeController(PostService postService) {
+        this.postService = postService;
+    }
+
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("posts", postService.getAllPosts());
         return "posts";
     }
 }
